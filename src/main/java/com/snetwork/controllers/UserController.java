e05df58cc41283eb21199f673dde02d373e503e2
Erik <gabriel.balbona.otero@gmail.com>
2018-03-07 23:19:27 +0100

    Added register page
    Added register page

---
+++ src/main/java/com/snetwork/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.snetwork.controllers;
+
+import com.snetwork.entities.User;
+import com.snetwork.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UsersService usersService;
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signup(@ModelAttribute @Validated User user, BindingResult result, Model model) {
+        //signUpFormValidator.validate(user, result);
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
+        //user.setRole(rolesService.getRoles()[0]);
+        usersService.addUser(user);
+        //securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
+        return "redirect:home";
+    }
+}
--- src/main/java/com/snetwork/services/UsersService.java
+++ src/main/java/com/snetwork/services/UsersService.java
@@ -3,10 +3,12 @@ package com.snetwork.services;
 import com.snetwork.entities.User;
 import com.snetwork.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
@@ -15,5 +17,9 @@ public class UsersService {
         List<User> users = new ArrayList<>();
         usersRepository.findAll().forEach(users::add);
         return users;
+    }
+
+    public void addUser(User user) {
+        usersRepository.save(user);
     }
 }
