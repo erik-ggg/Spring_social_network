d0aa2041f445ebab49d2834e5e63e5423f862e25
Erik <gabriel.balbona.otero@gmail.com>
2018-03-07 18:43:23 +0100

    Added gecko driver for firefox test

---
+++ src/main/java/com/snetwork/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.snetwork.utils;
+
+public class Constants {
+    public final static String BTN_PRIMARY_STRING = "btn btn-primary";
+    public final static String CLASS_STRING = "class";
+    public final static String LOGIN_STRING = "login";
+    public final static String SIGNUP_STRING = "signup";
+
+    public final static String TEXT_TYPE = "text";
+}
--- src/test/java/com/snetwork/SnetworkApplicationTests.java
+++ src/test/java/com/snetwork/SnetworkApplicationTests.java
@@ -1,16 +1,52 @@
 package com.snetwork;
 
-import org.junit.Test;
+import com.snetwork.pageobjects.PO_View;
+import com.snetwork.utils.Constants;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SnetworkApplicationTests {
+    static String  PathFirefox = "C:\\Users\\ERIK\\Documents\\University\\SDI\\Selenium\\Firefox46.0.win\\Firefox46.win\\FirefoxPortable.exe";
+    static WebDriver driver = getDriver(PathFirefox);
 
-	@Test
-	public void contextLoads() {
-	}
+    private static WebDriver getDriver(String path) {
+        System.setProperty("webdriver.gecko.driver", "F:\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        return driver;
+    }
 
+    static String URL = "http://localhost:8090";
+
+    @Before
+    public void setUp() throws Exception {
+        driver.navigate().to(URL);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.manage().deleteAllCookies();
+    }
+
+    @BeforeClass
+    static public void begin() {
+
+    }
+
+    @AfterClass
+    static public void end() {
+        driver.quit();
+    }
+
+    @Test
+    public void PR01() {
+        PO_View.checkElement(driver, Constants.TEXT_TYPE, "Social network");
+    }
 }
