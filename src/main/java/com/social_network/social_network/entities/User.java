79c6765ac2783205afc9cc6ea2f218f4201c25af
Erik <gabriel.balbona.otero@gmail.com>
2018-03-07 12:07:56 +0100

    Init project

---
+++ src/main/java/com/social_network/social_network/entities/User.java
@@ -0,0 +1,23 @@
+package com.social_network.social_network.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique=true)
+    private String email;
+    private String name;
+    private String password;
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+}
